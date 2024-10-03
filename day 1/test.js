545835e29112dc55d98c2cf73fa91a40d5dc7215
baharma <bagusaditya1899@gmail.com>
2024-10-04 04:45:58 +0800

    first commit

--- day 1/test.js
+++ day 1/test.js
@@ -1,4 +1,4 @@
-// Soal 2: Pembalik String
+// Soal 1: Pembalik String
 // Deskripsi: Buatlah fungsi yang membalikkan string yang diberikan. Misalnya, jika inputnya adalah "hello", maka outputnya harus "olleh".
 // Contoh Input:
 // plaintext
@@ -19,3 +19,29 @@
 
     const data = 'memek';
     console.log(reverseString(data));
+
+
+
+// Soal 3: Bilangan Genap dan Ganjil
+// Deskripsi: Buatlah program yang menerima sebuah array angka dan memisahkan angka genap dan ganjil ke dalam dua array terpisah.
+// Contoh Input:
+// plaintext
+
+
+// [1, 2, 3, 4, 5, 6]
+// Contoh Output:
+// plaintext
+
+
+// Genap: [2, 4, 6]
+// Ganjil: [1, 3, 5]
+
+function genap(arr){
+    return arr.filter(num =>num % 2 === 0);
+}
+
+function ganjil (arr){
+    return arr.filter(num => num % 2!== 0);
+}
+
+console.log(ganjil([1, 2, 3, 4, 5, 6]));
